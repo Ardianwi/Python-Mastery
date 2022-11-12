7f915a3aff22e074e41f6fc209504844f27a8dd7
Ardianwi <98331926+Ardianwi@users.noreply.github.com>
2022-11-12 21:58:19 +0700

    menghapus

--- Scrape/Scrape Detik/detik_scraper.py
+++ Scrape/Scrape Detik/detik_scraper.py
@@ -7,4 +7,17 @@ soup = BeautifulSoup(html_doc.text, 'html.parser')
 
 populer_area = soup.find(attrs={'class':'grid-row list-content'})
 
-print(populer_area)
+artikel = soup.findAll(attrs={'class':'list-content__item'})
+
+images = soup.findAll(attrs={'class':'media__image'})
+
+titles = soup.findAll(attrs={'class':'media__title'})
+
+text = soup.findAll(attrs={'class':'media__text'})
+
+for image in images:
+    print(image.find ('a').find('img')['title'])
+
+
+
+#print(images)
