57195423889dcb6c8b90f894cdccbfc2038ac1bb
Ardianwi <98331926+Ardianwi@users.noreply.github.com>
2022-11-12 08:44:25 +0700

    Scrape

--- Scrape/run.py
+++ Scrape/run.py
@@ -8,6 +8,19 @@ app =Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/detikpopuler')
+def detik_populer():
+    html_doc = requests.get('https://www.detik.com/terpopuler')
+
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+    populer_area = soup.find(attrs={'class': 'nhl indeks mgb-24'})
+
+    titles = populer_area.findAll(attrs={'class': 'media__title'})
+
+    image = populer_area.findAll(attrs={'class': 'media__image'})
+
+    return render_template('index.html', images = image)
 
 
 
